4f4edf8ca375c646bdd9c69dea73f9898c0492fb
farias-77 <gabrielfalves2003@gmail.com>
2022-08-09 10:23:50 -0300

    import reset.css e css global

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
-import { BrowserRouter, Router, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserPage from "./UserPage/UserPage.js";
 
 export default function App(){
     return(
         <BrowserRouter>
-            <Router>
-                <Route path="/user/:id" element={<UserPage />} />
-            </Router>
+            <Routes>
+                <Route path="/" element={<UserPage />} />
+            </Routes>
         </BrowserRouter>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import App from "./components/App.js";
 import { render } from "react-dom";
+import "./assets/reset.css";
+import "./assets/styles.css";
 
 render(<App />, document.querySelector(".root"));
