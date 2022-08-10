a53d792eee70285cb33b4289c4faed5d87a122f1
andrektozzi <andrektozzi@gmail.com>
2022-08-09 22:03:54 -0300

    feat: add signup page

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./Header/Header.js";
+import { useState } from "react";
+import SignUp from "./SignUpPage/signUpPage.js";
+import UserContext from "../contexts/UserContext.js";
 
 export default function App(){
+
+    const [user, setUser] = useState([]);
+
     return(
         <BrowserRouter>
-            <Header />
-            <Routes>
-                <Route path="/" element={<></>} />
-            </Routes>
+            <UserContext.Provider value={{ user, setUser }}>
+                <Header />
+                <Routes>
+                    <Route path="/signup" element={<SignUp/>} />
+                </Routes>
+            </UserContext.Provider>            
         </BrowserRouter>
     )
 }
