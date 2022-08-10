a2f1b2eed7542c639a1652c3055a1de63d073b62
Dimitri <assis.dimitri@engenharia.ufjf.br>
2022-08-10 16:25:04 -0300

    Tela /hashtah/:hashtag pronta

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import UserPage from "./UserPage/UserPage.js";
+
 import Header from "./Header/Header.js";
+import UserPage from "./UserPage/UserPage.js";
+import HashtagPage from "./HashtagPage/HashtagPage.js";
 
 export default function App(){
     return(
@@ -8,6 +10,7 @@ export default function App(){
             <Header />
             <Routes>
                 <Route path="/user/:id" element={<UserPage />} />
+                <Route path="/hashtag/:hashtag" element={<HashtagPage />} />
             </Routes>
         </BrowserRouter>
     )
---
+++ src/components/HashtagPage/HashtagPage.js
@@ -0,0 +1,86 @@
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import RealDataPostCard from "../RealDataPostCard.js";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function HashtagPage(){
+    const { hashtag } = useParams();
+    const [ hashtagPosts, setHashtagPosts ] = useState();
+
+    useEffect(() => {
+        const url = `http://localhost:5000/hashtag/${hashtag}`;
+        const promise = axios.get(url);
+
+        promise.then((res) => {
+            setHashtagPosts(res.data);
+        });
+
+        promise.catch((res) => {
+            alert(res.data);
+        })
+    }, []);
+
+    console.log(hashtagPosts)
+    return (
+        <Container>
+            <UserInfo>
+                <h2># {hashtag}</h2>
+            </UserInfo>
+            <div>
+                <Feed>
+                    {
+                        hashtagPosts === undefined ? <p>Ainda não existem posts com essa hashtag seja o primeiro.</p> :
+                        hashtagPosts.map((value,index)=><RealDataPostCard key={index} username={value.username} profilePicture={value.profilePicture} postText={value.postText} url={value.url}/>)
+                    }
+                </Feed>
+            </div>
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    margin-top: 72px;
+
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    
+    > div:last-child {
+        width: 50%;
+        display: flex;
+        justify-content: space-between;
+    }
+`;
+
+const Feed = styled.div`
+    width: 62%;
+    height: 200px;
+`;
+
+const UserInfo = styled.div`
+    width: 50%;
+
+    margin-top: 50px;
+    margin-bottom: 30px;
+
+    display: flex;
+    align-items: center;
+    
+    font-family: 'Oswald', sans-serif;
+    font-weight: 700;
+    font-size: 43px;
+    line-height: 64px;
+    color: #FFFFFF;
+
+
+    img{
+        width: 50px;
+        height: 50px;
+        
+        border-radius: 26.5px;
+        object-fit: cover;
+        margin-right: 20px;
+    }
+`;
